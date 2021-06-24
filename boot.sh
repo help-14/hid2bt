89a5378147315e0bd2c9afd0887fbd3ec751b165
Help-14 <help-14@hotmail.com>
2021-06-24 09:52:01 +0700

    fix path again

--- boot.sh
+++ boot.sh
@@ -2,12 +2,13 @@
 sudo hciconfig hci0 down
 sudo systemctl daemon-reload
 sudo /etc/init.d/bluetooth start
+#Go to work follder
+cd ~/hid2bt/
 # Update  mac address
 ./updateMac.sh
 #Update Name
 ./updateName.sh HID2BT
 #Get current Path
-cd /root/hid2bt/
 export C_PATH=$(pwd)
 
 cd $C_PATH/server && sudo ./btk_server.py &
